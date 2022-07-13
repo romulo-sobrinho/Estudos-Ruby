513e2b964c520f7e17fe3ea5e33c71b7882706fa
Romulo <romulosilva001@gmail.com>
2022-07-13 19:27:27 -0300

    Adicionado arquivo hash.rb

---
+++ hash.rb
@@ -0,0 +1,11 @@
+employee = {"name": "John", "salary": 5000}
+puts employee
+
+employee2 = {"name" => "John", "salary" => 5000}
+puts employee2
+
+employee3 = Hash.new
+employee3[:name] = "John"
+employee3[:salary] = 50000
+puts employee3
+ 
